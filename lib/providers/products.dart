39d76a43271c631cdd41203820b6c5e1c5a677bb
yunusyerli1 <yunus.yerli@gaboras.com>
2022-05-23 14:36:43 +0300

    Future with async await done on add-product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import './product.dart';
 
 class Products with ChangeNotifier {
@@ -64,17 +66,62 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct(Product product) {
-    final newProduct = Product(
-      id: DateTime.now().toString(),
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      //isFavorite: product.isFavorite,
-    );
-    _items.add(newProduct);
-    notifyListeners();
+  // Future<void> addProduct(Product product) {
+  //   final url = Uri.parse(
+  //       'https://flutter-update-cab8d-default-rtdb.firebaseio.com/products.json');
+  //   return http
+  //       .post(url,
+  //           body: json.encode({
+  //             'title': product.title,
+  //             'description': product.description,
+  //             'imageUrl': product.imageUrl,
+  //             'price': product.price,
+  //             'isFavorite': product.isFavorite,
+  //           }))
+  //       .then((response) {
+  //     final newProduct = Product(
+  //       id: json.decode(response.body)['name'],
+  //       title: product.title,
+  //       description: product.description,
+  //       price: product.price,
+  //       imageUrl: product.imageUrl,
+  //     );
+  //     _items.add(newProduct);
+  //     notifyListeners();
+  //   }).catchError((error) {
+  //     print(error);
+  //     throw error;
+  //   });
+  // }
+
+  Future<void> addProduct(Product product) async {
+    final url = Uri.parse(
+        'https://flutter-update-cab8d-default-rtdb.firebaseio.com/products.json');
+
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'title': product.title,
+            'description': product.description,
+            'imageUrl': product.imageUrl,
+            'price': product.price,
+            'isFavorite': product.isFavorite,
+          }));
+
+      final newProduct = Product(
+        id: json.decode(response.body)['name'],
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        //isFavorite: product.isFavorite,
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw error;
+    }
   }
 
   void updateProduct(String id, Product newProduct) {
